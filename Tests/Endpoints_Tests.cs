3b2ae4cd0b3d84f062f36cac425c3684dbed6907
JWMB <jonas.beckeman@gmail.com>
2024-05-28 07:06:23 +0200

    mocked endpoint test

--- Tests/Endpoints_Tests.cs
+++ Tests/Endpoints_Tests.cs
@@ -1,6 +1,9 @@
+using FakeItEasy;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using System.Net.Http.Json;
+using WebApi;
 
 namespace Tests
 {
@@ -29,6 +32,30 @@ namespace Tests
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadFromJsonAsync<Response>();
             result.Min.ShouldBe(1);
+        }
+
+        [Fact]
+        public async Task Zone_Get_Mocked()
+        {
+            // Arrange
+            var client = Utils.CreateClient(factory, services => {
+                var service = A.Fake<IElectricyPriceService>();
+                A.CallTo(() => service.Get(A<DateTime>._, A<string>._))
+                    .Returns(Task.FromResult(new List<ElectricyPriceRecord> { new ElectricyPriceRecord(1, 99, 1, DateTime.Today.Date, DateTime.Today.Date.AddHours(1)) }));
+                services.AddScoped<IElectricyPriceService>(sp => service);
+            });
+
+            var date = DateTime.Today;
+            var zone = "SE1";
+
+            // Act
+            var response = await client.GetAsync($"/electricityprice_zone?date={date:yyyy-MM-dd}&zone={zone}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadFromJsonAsync<Response>();
+            content.Max.ShouldBe(99);
         }
     }
 }
---
+++ Tests/Utils.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    internal class Utils
+    {
+        public static HttpClient CreateClient(WebApplicationFactory<Program> factory, Action<IServiceCollection> setup)
+        {
+            return factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services => setup(services));
+            }).CreateClient();
+        }
+    }
+}
