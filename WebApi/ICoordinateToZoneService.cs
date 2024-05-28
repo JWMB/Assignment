ec7fb3e284339f11b8c01f88c0e626b49ffab521
JWMB <jonas.beckeman@gmail.com>
2024-05-28 12:39:17 +0200

    Convert to sensible format

--- WebApi/ICoordinateToZoneService.cs
+++ WebApi/ICoordinateToZoneService.cs
@@ -18,6 +18,35 @@ namespace WebApi
             var found = polys.SingleOrDefault(o => o.Item2.IsPointInside(new((double)coordinate.Longitude, (double)coordinate.Latitude)));
             return Task.FromResult(found.Item2 == default ? null : found.Item1);
         }
+
+        public record Converted(string Id, List<Polygon> Polygons)
+        {
+            public static Converted Create(Feature feature)
+            {
+                return new Converted(
+                    $"SE{feature.Id}",
+                    (feature.Geometry.Type == "MultiPolygon" 
+                        ? feature.Geometry.Coordinates.SelectMany(CreateMultiPolygons)
+                        : feature.Geometry.Coordinates.Select(CreatePolygon)
+                    ).ToList()
+                    );
+
+                List<Polygon> CreateMultiPolygons(List<List<object>> data) =>
+                    //data.Select(o => new Polygon(o.Select(p => p as List<object>).Select(p => new[] { p[0], p[1] }.Cast<JsonElement>().Cast<object>().ToList()).Select(x => CreatePoint(x)))).ToList();
+                    data.Select(o => CreatePolygon(o.OfType<List<object>>().ToList())).ToList();
+
+                Polygon CreatePolygon(List<List<object>> points) => 
+                    new Polygon(points.Select(CreatePoint));
+
+                Point CreatePoint(List<object> values)
+                {
+                    var cast = values.Cast<JsonElement>().ToList();
+                    return new Point(cast[0].GetDouble(), cast[1].GetDouble());
+                }
+            }
+            public bool IsInside(Point pt) => Polygons.Any(o => o.IsPointInside(pt));
+        }
+
         // from https://json2csharp.com/
         public record Feature(
            [property: JsonPropertyName("type")] string Type,
