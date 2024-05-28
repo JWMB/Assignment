69526b719f4a7d685ed0c69edd95ed1295e9fe31
JWMB <jonas.beckeman@gmail.com>
2024-05-28 06:57:28 +0200

    IElectricyPriceService

---
+++ WebApi/IElectricyPriceService.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApi
+{
+    public interface IElectricyPriceService
+    {
+        Task<List<ElectricyPriceRecord>> Get(DateTime date, string zone);
+    }
+
+    // generated using https://json2csharp.com/
+    public readonly record struct ElectricyPriceRecord(
+        double SEK_per_kWh,
+        double EUR_per_kWh,
+        double EXR,
+        DateTime time_start,
+        DateTime time_end
+    );
+
+    public class ElectricyPriceService : IElectricyPriceService
+    {
+        public Task<List<ElectricyPriceRecord>> Get(DateTime date, string zone)
+        {
+            return Task.FromResult(new[] { new ElectricyPriceRecord(1, 1, 1, DateTime.Today, DateTime.Today.AddHours(1)) }.ToList());
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IElectricyPriceService, ElectricyPriceService>();
 
 var app = builder.Build();
 
@@ -16,10 +21,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/electricityprice_zone", (
+app.MapGet("/electricityprice_zone", async ([FromServices] IElectricyPriceService priceService,
     DateTime date, string zone) =>
 {
-    return new Response(1, 1, 1, "SEK");
+    var items = await priceService.Get(date, zone);
+    return new Response(
+        (decimal)items.Min(o => o.EUR_per_kWh),
+        (decimal)items.Max(o => o.EUR_per_kWh),
+        (decimal)items.Average(o => o.EUR_per_kWh),
+        "EUR");
 })
 .WithOpenApi();
 
