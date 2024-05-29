f4a7ece5609cab1c30e996488a11af154e8e30f1
JWMB <jonas.beckeman@gmail.com>
2024-05-29 06:52:13 +0200

    more tests

--- Tests/CoordinateToZoneService_Tests.cs
+++ Tests/CoordinateToZoneService_Tests.cs
@@ -28,6 +28,7 @@ namespace Tests
         [InlineData(55.597203911235034, 13.0017964889469, "SE4")] // Malmö
         [InlineData(67.12664979623472, 20.633174522636434, "SE1")] // Gällivare
         [InlineData(61.72033675223663, 17.051631721240916, "SE2")] // Hudiksvall
+        [InlineData(57.341842859691461, 13.588611315673878, null)] // Sebjörnarp - free energy? :)
         [InlineData(0, 0, null)]
         public async Task CoordinateToZoneService_RealZones(double longitude, double latitude, string? expectedZone)
         {
@@ -36,6 +37,25 @@ namespace Tests
             var zone = await sut.Get(new((decimal)longitude, (decimal)latitude));
             zone.ShouldBe(expectedZone);
         }
+
+        [Fact]
+        public async Task CoordinateToZoneService_FindHole()
+        {
+            var zoneDefinitionProvider = new GeoJsonZoneDefinitionProvider();
+            var sut = new CoordinateToZoneService(zoneDefinitionProvider);
+            var ptCenter = (57.361842859691464, 13.738611315673879);
+            var steps = 30;
+            var stepSize = 0.01;
+            var range = Enumerable.Range(-steps / 2, steps);
+            var points = range.SelectMany(x => range.Select(y => (stepSize * x + ptCenter.Item1, stepSize * y + ptCenter.Item2)));
+            foreach (var point in points)
+            {
+                var zone = await sut.Get(new((decimal)point.Item1, (decimal)point.Item2));
+                if (zone == null)
+                    throw new Exception($"Here be monsters: {point}");
+            }
+        }
+
 
         [Fact]
         public async Task GeoJsonZoneDefinitionProvider_AllValid()
